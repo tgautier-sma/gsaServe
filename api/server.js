5cda7f3c6647a94271099aeb5ac241e7aae10b3f
Thierry GAUTIER <thierry_gautier@groupe-sma.fr>
2023-04-05 06:25:57 +0200

    Update

--- api/server.js
+++ api/server.js
@@ -1,24 +1,33 @@
 // index.js
 const express = require('express')
+const bodyParser = require('body-parser');
 
 const app = express()
-const PORT = 4000
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+app.use(bodyParser.text());
+/* const PORT = 4000
 
 app.listen(PORT, () => {
     console.log(`API listening on PORT ${PORT} `)
 })
-
-app.get('/', (req, res) => {
+ */
+app.get('/api', (req, res) => {
     res.send('Hey this is my API running 🥳')
 })
 
-app.get('/about', (req, res) => {
+app.get('/api/about', (req, res) => {
     res.send('This is my about route..... ')
 })
-app.post('/event', (req, res) => {
+app.post('/api/event', (req, res) => {
     console.info(req.body);
     res.send(
-        { msg: 'POST EVENT request received. See Log for data received.', data: req.body }
+        { msg: 'POST EVENT request received. See Log for data received.', 
+        query: req.query,
+        params: req.params,
+        body:req.body
+     }
     );
 })
 
