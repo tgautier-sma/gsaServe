1a3962c402732fb3b4a5a1ca893ce5f733d5c4fe
a13246 <thierry_gautier@groupe-sma.fr>
2023-04-12 16:31:23 +0200

    update

--- api/server.js
+++ api/server.js
@@ -1,8 +1,30 @@
 // server.js
+const env = process.env;
 const express = require('express');
 const helmet = require("helmet");
 const bodyParser = require('body-parser');
 // const db = require('../lib/db');
+// logging system
+const winston = require('winston');
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'gsa-serve' },
+    transports: [
+        //
+        // - Write all logs with importance level of `error` or less to `error.log`
+        // - Write all logs with importance level of `info` or less to `combined.log`
+        //
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combined.log' }),
+    ],
+});
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple(),
+    }));
+}
+
 
 const app = express();
 app.use(helmet());
@@ -10,6 +32,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
 app.use(bodyParser.text());
+logger.info("System launch");
 
 app.get('/api', (req, res) => {
     res.send('Hey ! this is my API running 🥳')
@@ -27,11 +50,12 @@ app.get('/api/connect', (req, res) => {
 app.post('/api/event', (req, res) => {
     console.info(req.body);
     res.send(
-        { msg: 'POST EVENT request received. See Log for data received.', 
-        query: req.query,
-        params: req.params,
-        body:req.body
-     }
+        {
+            msg: 'POST EVENT request received. See Log for data received.',
+            query: req.query,
+            params: req.params,
+            body: req.body
+        }
     );
 })
 
