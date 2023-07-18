21d095c6aae18aa1c7ef601b2e6622a0fbddfa19
Thierry GAUTIER <thierry_gautier@smabtp.fr>
2023-07-18 11:36:10 +0200

    Update

--- api/server.js
+++ api/server.js
@@ -53,6 +53,7 @@ app.post('/api/event', (req, res) => {
     res.send(
         {
             msg: 'POST EVENT request received. See Log for data received.',
+            ts: new Date(),
             query: req.query,
             params: req.params,
             body: req.body
