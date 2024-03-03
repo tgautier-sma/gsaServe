46d122ad293b1ed83307f07d5358c5e181242797
Thierry GAUTIER <thierry_gautier@groupe-sma.fr>
2024-03-03 23:57:35 +0100

    Update

--- dist/api/tools/tools.js
+++ dist/api/tools/tools.js
@@ -1,27 +1,25 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.fetchMetaTags = void 0;
 const request_1 = require("../../request");
-const cheerio_1 = __importDefault(require("cheerio"));
+// import cheerio from 'cheerio';
 const fetchMetaTags = async (url) => {
     return new Promise((resolve, reject) => {
         const ds = new Date().getTime();
         (0, request_1.get)(url)
             .then((data) => {
             // console.log(data);
-            const $ = cheerio_1.default.load(data);
             const metaTags = {};
-            $('meta').each((i, element) => {
+            /* const $ = cheerio.load(data);
+            $('meta').each((i: any, element: any) => {
                 const name = $(element).attr('name')
                     || $(element).attr('property')
-                    || $(element).attr('http-equiv');
+                    || $(element).attr('http-equiv')
+                    || $(element).attr('Content-Security-Policy');
                 if (name) {
                     metaTags[name] = $(element).attr('content');
                 }
-            });
+            }); */
             const de = new Date().getTime();
             resolve({
                 url: url,
