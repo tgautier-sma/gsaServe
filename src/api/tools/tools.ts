1c710ab5c4ede4c898f711760af322b9889e4efa
Thierry GAUTIER <thierry_gautier@groupe-sma.fr>
2024-03-03 23:57:59 +0100

    Update

--- src/api/tools/tools.ts
+++ src/api/tools/tools.ts
@@ -1,5 +1,5 @@
 import { get } from "../../request"
-import cheerio from 'cheerio';
+// import cheerio from 'cheerio';
 
 export const fetchMetaTags = async (url: string) => {
     return new Promise((resolve, reject) => {
@@ -7,17 +7,18 @@ export const fetchMetaTags = async (url: string) => {
         get(url)
             .then((data: any) => {
                 // console.log(data);
-                const $ = cheerio.load(data);
                 const metaTags: any = {};
+                
+                /* const $ = cheerio.load(data);
                 $('meta').each((i: any, element: any) => {
                     const name = $(element).attr('name')
                         || $(element).attr('property')
                         || $(element).attr('http-equiv')
-                        /* || $(element).attr('Content-Security-Policy') */;
+                        || $(element).attr('Content-Security-Policy');
                     if (name) {
                         metaTags[name] = $(element).attr('content');
                     }
-                });
+                }); */
                 const de = new Date().getTime();
                 resolve({
                     url: url,
